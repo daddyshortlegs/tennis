3e4e6a54395e151b99eb9f121b1fa506609c1de0
andys <andysmith836@gmail.com>
2020-04-14 23:35:11 +0100

    Slight reorder

--- Tennis/TennisScoreCalculator.swift
+++ Tennis/TennisScoreCalculator.swift
@@ -15,13 +15,13 @@ class TennisScoreCalculator {
   private func isAdvantageOrWinner(_ player1Points: Int, _ player2Points: Int) -> Bool {
     max(player1Points, player2Points) >= 4 && player1Points != player2Points
   }
-  
-  private func winningPlayer(_ player1Points: Int, _ player2Points: Int) -> String {
-    (player1Points > player2Points ? "1" : "2")
-  }
-    
+
   private func playerStatus(_ player1Points: Int, _ player2Points: Int) -> String {
     abs(player1Points - player2Points) == 1 ? "Advantage" : "Win for"
+  }
+
+  private func winningPlayer(_ player1Points: Int, _ player2Points: Int) -> String {
+    (player1Points > player2Points ? "1" : "2")
   }
   
   private func pointToScore(_ points: Int) -> String {

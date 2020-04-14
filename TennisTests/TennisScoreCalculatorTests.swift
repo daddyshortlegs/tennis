f5b115e67a50a01657eeef83e84fdf5afe73f691
andys <andysmith836@gmail.com>
2020-04-14 22:04:09 +0100

    Make tests more readable

--- TennisTests/TennisScoreCalculatorTests.swift
+++ TennisTests/TennisScoreCalculatorTests.swift
@@ -9,44 +9,45 @@ class TennisScoreCalculatorTests: XCTestCase {
   }
 
   func testPlayer1() throws {
-    XCTAssertEqual("Fifteen-Love", calc.score(player1Points: 1, player2Points: 0))
-    XCTAssertEqual("Thirty-Love", calc.score(player1Points: 2, player2Points: 0))
-    XCTAssertEqual("Forty-Love", calc.score(player1Points: 3, player2Points: 0))
+    verify(p1Points: 1, p2Points: 0, score: "Fifteen-Love")
+    verify(p1Points: 2, p2Points: 0, score: "Thirty-Love")
+    verify(p1Points: 3, p2Points: 0, score: "Forty-Love")
   }
-
+  
   func testPlayer2() throws {
-    XCTAssertEqual("Love-Fifteen", calc.score(player1Points: 0, player2Points: 1))
-    XCTAssertEqual("Love-Thirty", calc.score(player1Points: 0, player2Points: 2))
-    XCTAssertEqual("Love-Forty", calc.score(player1Points: 0, player2Points: 3))
+    verify(p1Points: 0, p2Points: 1, score: "Love-Fifteen")
+    verify(p1Points: 0, p2Points: 2, score: "Love-Thirty")
+    verify(p1Points: 0, p2Points: 3, score: "Love-Forty")
   }
   
   func testPlayer1Winner() {
-    XCTAssertEqual("Win for player 1", calc.score(player1Points: 4, player2Points: 0))
-    XCTAssertEqual("Win for player 1", calc.score(player1Points: 4, player2Points: 1))
-    XCTAssertEqual("Win for player 1", calc.score(player1Points: 4, player2Points: 2))
+    verify(p1Points: 4, p2Points: 0, score: "Win for player 1")
+    verify(p1Points: 4, p2Points: 1, score: "Win for player 1")
+    verify(p1Points: 4, p2Points: 2, score: "Win for player 1")
   }
 
   func testPlayer2Winner() {
-    XCTAssertEqual("Win for player 2", calc.score(player1Points: 0, player2Points: 4))
-    XCTAssertEqual("Win for player 2", calc.score(player1Points: 1, player2Points: 4))
-    XCTAssertEqual("Win for player 2", calc.score(player1Points: 2, player2Points: 4))
+    verify(p1Points: 0, p2Points: 4, score: "Win for player 2")
+    verify(p1Points: 1, p2Points: 4, score: "Win for player 2")
+    verify(p1Points: 2, p2Points: 4, score: "Win for player 2")
   }
   
   func testAdvantage() {
-    XCTAssertEqual("Advantage player 1", calc.score(player1Points: 4, player2Points: 3))
-    XCTAssertEqual("Advantage player 1", calc.score(player1Points: 8, player2Points: 7))
-    XCTAssertEqual("Advantage player 2", calc.score(player1Points: 3, player2Points: 4))
-    XCTAssertEqual("Advantage player 2", calc.score(player1Points: 7, player2Points: 8))
+    verify(p1Points: 4, p2Points: 3, score: "Advantage player 1")
+    verify(p1Points: 8, p2Points: 7, score: "Advantage player 1")
+    verify(p1Points: 3, p2Points: 4, score: "Advantage player 2")
+    verify(p1Points: 7, p2Points: 8, score: "Advantage player 2")
   }
   
   func testTie() {
-    XCTAssertEqual("Love-All", calc.score(player1Points: 0, player2Points: 0))
-    XCTAssertEqual("Fifteen-All", calc.score(player1Points: 1, player2Points: 1))
-    XCTAssertEqual("Thirty-All", calc.score(player1Points: 2, player2Points: 2))
-    XCTAssertEqual("Deuce", calc.score(player1Points: 3, player2Points: 3))
-    XCTAssertEqual("Deuce", calc.score(player1Points: 4, player2Points: 4))
-
+    verify(p1Points: 0, p2Points: 0, score: "Love-All")
+    verify(p1Points: 1, p2Points: 1, score: "Fifteen-All")
+    verify(p1Points: 2, p2Points: 2, score: "Thirty-All")
+    verify(p1Points: 3, p2Points: 3, score: "Deuce")
+    verify(p1Points: 4, p2Points: 4, score: "Deuce")
   }
-
-
+  
+  func verify(p1Points: Int, p2Points: Int, score: String) {
+    XCTAssertEqual(score, calc.score(player1Points: p1Points, player2Points: p2Points))
+  }
 }

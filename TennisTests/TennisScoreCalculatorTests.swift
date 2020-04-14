d4927a5512dea401a51da8f4039397f9dc68a15c
andys <andysmith836@gmail.com>
2020-04-14 22:07:44 +0100

    Make tests more complete

--- TennisTests/TennisScoreCalculatorTests.swift
+++ TennisTests/TennisScoreCalculatorTests.swift
@@ -10,15 +10,19 @@ class TennisScoreCalculatorTests: XCTestCase {
 
   func testPlayer1() throws {
     verify(p1Points: 1, p2Points: 0, score: "Fifteen-Love")
+    verify(p1Points: 0, p2Points: 1, score: "Love-Fifteen")
     verify(p1Points: 2, p2Points: 0, score: "Thirty-Love")
+    verify(p1Points: 0, p2Points: 2, score: "Love-Thirty")
+    verify(p1Points: 2, p2Points: 1, score: "Thirty-Fifteen")
+    verify(p1Points: 1, p2Points: 2, score: "Fifteen-Thirty")
     verify(p1Points: 3, p2Points: 0, score: "Forty-Love")
+    verify(p1Points: 0, p2Points: 3, score: "Love-Forty")
+    verify(p1Points: 3, p2Points: 1, score: "Forty-Fifteen")
+    verify(p1Points: 1, p2Points: 3, score: "Fifteen-Forty")
+    verify(p1Points: 3, p2Points: 2, score: "Forty-Thirty")
+    verify(p1Points: 2, p2Points: 3, score: "Thirty-Forty")
   }
   
-  func testPlayer2() throws {
-    verify(p1Points: 0, p2Points: 1, score: "Love-Fifteen")
-    verify(p1Points: 0, p2Points: 2, score: "Love-Thirty")
-    verify(p1Points: 0, p2Points: 3, score: "Love-Forty")
-  }
   
   func testPlayer1Winner() {
     verify(p1Points: 4, p2Points: 0, score: "Win for player 1")

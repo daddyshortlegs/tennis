c6d2feb6a604ebf58b93d867c8fb5bc5962f0f4c
andys <andysmith836@gmail.com>
2020-04-14 23:09:37 +0100

    Remove elses

--- Tennis/TennisScoreCalculator.swift
+++ Tennis/TennisScoreCalculator.swift
@@ -2,7 +2,7 @@ class TennisScoreCalculator {
   func score(player1Points: Int, player2Points: Int) -> String {
     
 
-    let maxPoints = max(player1Points, player2Points)    
+    let maxPoints = max(player1Points, player2Points)
     if player1Points > player2Points && maxPoints >= 4 {
       return advantageOrWinner(player1Points, player2Points) + " player 1"
     }
@@ -11,17 +11,15 @@ class TennisScoreCalculator {
       return advantageOrWinner(player1Points, player2Points) + " player 2"
     }
 
-    var result = pointToScore(points: player1Points) + "-"
-
     if player1Points == player2Points {
       if player1Points >= 3 {
         return "Deuce"
-      } else {
-        result += "All"
       }
-    } else {
-      result += pointToScore(points: player2Points)
+      return pointToScore(player1Points) + "-All"
     }
+
+    var result = pointToScore(player1Points) + "-"
+    result += pointToScore(player2Points)
     return result
   }
 
@@ -29,19 +27,8 @@ class TennisScoreCalculator {
     abs(player1Points - player2Points) == 1 ? "Advantage" : "Win for"
   }
   
-  private func pointToScore(points: Int) -> String {
-    if points == 1 {
-      return "Fifteen"
-    }
-
-    if points == 2 {
-      return "Thirty"
-    }
-
-    if points == 3 {
-      return "Forty"
-    }
-
-    return "Love"
+  private func pointToScore(_ points: Int) -> String {
+    let scores = [0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"]
+    return scores[points] ?? "Love"
   }
 }

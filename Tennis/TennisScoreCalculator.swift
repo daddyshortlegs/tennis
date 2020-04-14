50ceeb1cb6f70c58916848cd1350afee8269a0d1
andys <andysmith836@gmail.com>
2020-04-14 21:08:51 +0100

    1 point

--- Tennis/TennisScoreCalculator.swift
+++ Tennis/TennisScoreCalculator.swift
@@ -1,5 +1,9 @@
 class TennisScoreCalculator {
   func score(player1Points: Int, player2Points: Int) -> String {
+    if player1Points == 1 {
+      return "Fifteen-Love"
+    }
+    
     return "Love-Love"
   }
 }
--- TennisTests/TennisScoreCalculatorTests.swift
+++ TennisTests/TennisScoreCalculatorTests.swift
@@ -8,11 +8,14 @@ class TennisScoreCalculatorTests: XCTestCase {
     calc = TennisScoreCalculator()
   }
 
-  func testScore() throws {
+  func testNoPoints() throws {
     let result = calc.score(player1Points: 0, player2Points: 0)
     XCTAssertEqual("Love-Love", result)
+  }
 
-      
+  func testOnePoint() throws {
+    let result = calc.score(player1Points: 1, player2Points: 0)
+    XCTAssertEqual("Fifteen-Love", result)
   }
 
 
